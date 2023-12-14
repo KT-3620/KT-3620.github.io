954862ad6b6271448f7d1543a29a26b40d93077d
KT-3620 <106292417+KT-3620@users.noreply.github.com>
2023-12-14 12:25:06 +0000

    フッターの更新

---
+++ components/app/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-blue-100 dark:bg-blue-950 h-20 text-black dark:text-white justify-center content-center flex rounded-lg"
+  >
+    <a
+      class="items-center active:underline hovor:text-black hovor:dark:text-white dark:text-gray-300 text-gray-700 flex"
+      href="https://github.com/KT-3620"
+      target="_blank"
+    >
+      <p>@KT-3620</p>
+      <Icon class="h-8 m-1" name="ph:arrow-circle-right" />
+    </a>
+  </div>
+</template>
+<script setup></script>
