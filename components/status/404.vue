68cec8cdfa6dac6ee37e45389c2efb235cfaefd0
KT-3620 <106292417+KT-3620@users.noreply.github.com>
2023-12-16 06:40:47 +0000

    404コンポーネントの追加

---
+++ components/status/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="p-3 text-6xl content-center bg-stone-500 font-bold text-center">
+    404
+  </h1>
+  <h1 class="p-3 text-2xl content-center font-bold text-center">Not Found</h1>
+  <!-- Back to home -->
+</template>
+
+<script setup></script>
