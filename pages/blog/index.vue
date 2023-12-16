03405701657b477a7b84fc9ddb0dc8fe87cf0b8e
KT-3620 <kt.app.support@icloud.com>
2023-12-17 01:09:54 +0900

    記事リストの整形

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="p-3">
-    <h1 class="text-4xl font-bold">ブログ</h1>
+    <h1 class="p-3 text-4xl font-bold">ブログ</h1>
     <ContentList class="w-full" path="/blog" v-slot="{ list }">
       <div class="grid grid-cols-1 lg:grid-cols-2">
         <NuxtLink
           v-for="blog in list"
           :key="blog._path"
-          class="font-lg m-1 rounded-xl bg-gray-100 p-8 dark:bg-gray-800"
+          class="m-1 flex flex-col rounded-xl bg-gray-100 p-8 dark:bg-gray-800"
           :to="blog._path"
-          >{{ blog.title }}
+        >
+          <h1 class="text-xl font-bold">
+            {{ blog.title }}
+          </h1>
+          <p>
+            {{ blog.description }}
+          </p>
         </NuxtLink>
       </div>
     </ContentList>
