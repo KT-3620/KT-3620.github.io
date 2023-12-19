ec0803fdc73a653bdcbea78904e1ae315057af66
KT-3620 <kt.app.support@icloud.com>
2023-12-19 21:32:57 +0900

    プライバシーポリシーを追加

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,14 +1,21 @@
 <template>
   <div
-    class="flex h-20 content-center justify-center rounded-lg bg-blue-100 text-black dark:bg-blue-950 dark:text-white"
+    class="flex flex-col rounded-lg bg-blue-100 p-3 text-center dark:bg-blue-950"
   >
     <a
-      class="hovor:text-black hovor:dark:text-white flex items-center text-gray-700 active:underline dark:text-gray-300"
+      class="flex content-center items-center justify-center p-1 text-gray-700 active:underline dark:text-gray-300"
       href="https://github.com/KT-3620"
       target="_blank"
     >
       <p>@KT-3620</p>
       <Icon class="m-1 h-8" name="ph:arrow-circle-right" />
     </a>
+    <NuxtLink to="/privacy" class="p-1">
+      <p
+        class="text-sm text-gray-500 underline active:underline dark:text-gray-400"
+      >
+        プライバシーポリシー
+      </p>
+    </NuxtLink>
   </div>
 </template>
